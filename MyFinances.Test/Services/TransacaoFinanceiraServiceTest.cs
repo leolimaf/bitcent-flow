fa349e5a5bc5178efd69b7d3cdb4d3029652fa16
Leonardo Lima <leolimaf@outlook.com>
2023-10-31 21:07:01 -0300

    ajustes nos testes

--- MyFinances.Test/Services/TransacaoFinanceiraServiceTest.cs
+++ MyFinances.Test/Services/TransacaoFinanceiraServiceTest.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using MyFinances.API.Services.Interfaces;
 using MyFinances.Domain.DTOs.TransacaoFinanceira;
 using MyFinances.Domain.Models;
+using MyFinances.Useful.Date;
 
 namespace MyFinances.Test.Services;
 
@@ -20,8 +21,8 @@ public class TransacaoFinanceiraServiceTest : IClassFixture<TestFixture>
         var transacaoDto = new CreateTransacaoDTO
         {
             Descricao = "Aluguel",
-            Valor = 930,
-            Data = DateTime.Today,
+            Valor = 930.50,
+            Data = DataInterna.ObterHorarioDeBrasilia(),
             Tipo = TipoTransacao.DESPESA
         };
         
@@ -42,15 +43,24 @@ public class TransacaoFinanceiraServiceTest : IClassFixture<TestFixture>
     }
 
     [Fact]
-    public void TestarObterTransacaoPorId()
+    public void TestarObterTransacaoExistentePorId()
     {
-        var transacao = _transacaoFinanceiraService.ObterTransacaoPorId(Guid.NewGuid());
+        var transacao = _transacaoFinanceiraService.ObterTransacaoPorId(new Guid("6aee466f-f10e-4fa8-94d8-fe02a4c7613f"));
         Assert.NotNull(transacao);
     }
     
+    [Fact]
+    public void TestarObterTransacaoInexistentePorId()
+    {
+        var transacao = _transacaoFinanceiraService.ObterTransacaoPorId(new Guid("5b411039-b60d-4f8b-bc55-80eb90af53a3"));
+        Assert.Null(transacao);
+    }
+    
     [Theory]
-    [InlineData("35d036d0-429e-4102-88ac-08a7af2ba92f"), InlineData("6ada8c22-0235-4595-8fde-34bfab840ce2"), InlineData("2b26dbba-6e17-468e-a411-ff77c0ca2a68")]
-    public void TestarObterTransacaoPorVariosIds(string id)
+    [InlineData("6aee466f-f10e-4fa8-94d8-fe02a4c7613f")]
+    [InlineData("15901a48-f791-4175-bc4a-e7bac7edd065")] 
+    [InlineData("a78377f9-ceb7-4aa7-8b5f-34ff35004754")]
+    public void TestarObterTransacaoExistentePorVariosIds(string id)
     {
         var transacao = _transacaoFinanceiraService.ObterTransacaoPorId(new Guid(id));
         Assert.NotNull(transacao);
@@ -59,7 +69,8 @@ public class TransacaoFinanceiraServiceTest : IClassFixture<TestFixture>
     [Fact]
     public void TestarAtualizarTransacao()
     {
-        var transacao = _transacaoFinanceiraService.ObterTransacaoPorId(new Guid("35d036d0-429e-4102-88ac-08a7af2ba92f"));
+        // ARRANGE
+        var transacao = _transacaoFinanceiraService.ObterTransacaoPorId(new Guid("6aee466f-f10e-4fa8-94d8-fe02a4c7613f"));
         
         var transacaoDto = new UpdateTransacaoDTO
         {
@@ -69,7 +80,10 @@ public class TransacaoFinanceiraServiceTest : IClassFixture<TestFixture>
             Tipo = transacao.Tipo
         };
 
+        // ACT
         var result = _transacaoFinanceiraService.AtualizarTransacao(new Guid("35d036d0-429e-4102-88ac-08a7af2ba92f"), transacaoDto);
+        
+        // ASSERT
         Assert.True(result.IsSuccess);
     }
 
--- MyFinances.Test/TestFixture.cs
+++ MyFinances.Test/TestFixture.cs
@@ -31,7 +31,9 @@ public class TestFixture : IDisposable
 
     private void SeedData()
     {
-        DbContext.TransacoesFinanceiras.Add(new() {Id = Guid.NewGuid(), Data = DateTime.Now.AddDays(-1), Descricao = "Conta de luz", Valor = 136.25, Tipo = TipoTransacao.DESPESA, IdUsuario = new Guid("06722053-90c6-416c-adab-3d69fd8f6c0d")});
+        DbContext.TransacoesFinanceiras.Add(new() {Id = new Guid("6aee466f-f10e-4fa8-94d8-fe02a4c7613f"), Data = DateTime.Now.AddDays(-1), Descricao = "Conta de luz", Valor = 136.25, Tipo = TipoTransacao.DESPESA, IdUsuario = new Guid("faae087f-6a08-447e-a311-e43009793f05")});
+        DbContext.TransacoesFinanceiras.Add(new() {Id = new Guid("15901a48-f791-4175-bc4a-e7bac7edd065"), Data = DateTime.Now.AddDays(-1), Descricao = "Almoço", Valor = 26.15, Tipo = TipoTransacao.DESPESA, IdUsuario = new Guid("06722053-90c6-416c-adab-3d69fd8f6c0d")});
+        DbContext.TransacoesFinanceiras.Add(new() {Id = new Guid("a78377f9-ceb7-4aa7-8b5f-34ff35004754"), Data = DateTime.Now.AddDays(-1), Descricao = "Academia", Valor = 110.00, Tipo = TipoTransacao.DESPESA, IdUsuario = new Guid("06722053-90c6-416c-adab-3d69fd8f6c0d")});
         DbContext.TransacoesFinanceiras.Add(new() {Id = Guid.NewGuid(), Data = DateTime.Now.AddHours(-6), Descricao = "Salário", Valor = 2575.00, Tipo = TipoTransacao.RECEITA, IdUsuario = new Guid("06722053-90c6-416c-adab-3d69fd8f6c0d")});
         DbContext.TransacoesFinanceiras.Add(new() {Id = Guid.NewGuid(), Data = DateTime.Now, Descricao = "Conta de água", Valor = 83.65, Tipo = TipoTransacao.DESPESA, IdUsuario = new Guid("06722053-90c6-416c-adab-3d69fd8f6c0d")});
         DbContext.SaveChanges();
