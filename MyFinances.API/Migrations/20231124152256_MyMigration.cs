6259225f3eaf8772f759807852e5bd8e574e877d
Leonardo Lima <leolimaf@outlook.com>
2023-11-24 15:30:03 -0300

    testes utilizando TestContainers

---
+++ MyFinances.API/Migrations/20231124152256_MyMigration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFinances.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "USUARIO",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NOME = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EMAIL = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SENHA_HASH = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TOKEN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VALIDADE_TOKEN = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ADMINISTRADOR = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USUARIO", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TRANSACAO_FINANCEIRA",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DESCRICAO = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DATA = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VALOR = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TIPO = table.Column<int>(type: "int", nullable: false),
+                    ID_USUARIO = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TRANSACAO_FINANCEIRA", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TRANSACAO_FINANCEIRA_USUARIO_ID_USUARIO",
+                        column: x => x.ID_USUARIO,
+                        principalTable: "USUARIO",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TRANSACAO_FINANCEIRA_ID_USUARIO",
+                table: "TRANSACAO_FINANCEIRA",
+                column: "ID_USUARIO");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_USUARIO_EMAIL",
+                table: "USUARIO",
+                column: "EMAIL",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_USUARIO_NOME",
+                table: "USUARIO",
+                column: "NOME",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TRANSACAO_FINANCEIRA");
+
+            migrationBuilder.DropTable(
+                name: "USUARIO");
+        }
+    }
+}
